614f48456cd1299a69c6666142205282e99966db
BUFFMOMO <projectinfo1105@gmail.com>
2022-02-28 18:11:49 +0545

    static  urls added

--- paluwa/settings.py
+++ paluwa/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/example.com/static/"
 
 MEDIA_URL = '/images/'
 
--- paluwa/urls.py
+++ paluwa/urls.py
@@ -26,5 +26,5 @@ urlpatterns = [
     path('', include('main.urls')  ),
       url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
